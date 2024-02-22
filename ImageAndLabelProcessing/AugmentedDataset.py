50d1adb6b363bbf03f8b859213c974575e3488d4
jakub-czerniak <j.deadyet@protonmail.com>
2024-02-22 15:50:54 +0100

    Finished augmented dataset

--- ImageAndLabelProcessing/AugmentedDataset.py
+++ ImageAndLabelProcessing/AugmentedDataset.py
@@ -2,7 +2,7 @@ import shutil
 import sys
 import os
 import numpy as np
-from PIL import Image, ImageOps
+from PIL import Image, ImageEnhance
 import cv2 as cv
 import random
 from ImageAndLabelProcessing.BaseDatasetsProcessing import drop_splits_target_folders, create_splits_target_folders, save_labels_to_txt
@@ -22,6 +22,13 @@ def convert_to_pil(image_cv):
 
 
 def copy_base_page_label(image_name, target_pages_folder, split_folder):
+    label_name = image_name.replace('.jpg', '.txt')
+    base_label_path = os.path.join('../Data/Pages/Base/labels/', split_folder, label_name)
+    target_label_path = os.path.join(target_pages_folder, 'labels', split_folder, label_name)
+    shutil.copyfile(base_label_path, target_label_path)
+
+
+def copy_base_page_image(image_name, target_pages_folder, split_folder):
     base_image_path = os.path.join('../Data/Pages/Base/images/', split_folder, image_name)
     target_image_path = os.path.join(target_pages_folder, 'images', split_folder, image_name)
     shutil.copyfile(base_image_path, target_image_path)
@@ -32,64 +39,70 @@ def salt_and_pepper_noise(image, noise_percent=0.005, s_to_p_ratio=0.5):
 
     salt_count = int(noise_percent * s_to_p_ratio * image.size)
     for i in range(salt_count):
-        x = random.randint(0, w)
-        y = random.randint(0, h)
+        x = random.randint(0, w-1)
+        y = random.randint(0, h-1)
         image[y][x] = 255
 
     pepper_count = int(noise_percent * (1-s_to_p_ratio) * image.size)
     for i in range(pepper_count):
-        x = random.randint(0, w)
-        y = random.randint(0, h)
+        x = random.randint(0, w-1)
+        y = random.randint(0, h-1)
         image[y][x] = 0
 
     return image
 
 
 def resize_polygons(polygons, ratio):
-    polygons = polygons * ratio
+    polygons = (np.asarray(polygons) * ratio).astype(int)
     return polygons
 
 
 def random_augmentation(image_path):
     image = cv.imread(image_path)
-    random_int = np.random.randint(0, 6)
+    random_aug = np.random.choice(['rotation', 'noise', 'blur', 'downsizing', 'removing rows, columns', 'saturation', 'brightness', 'contrast'])
     img_name = os.path.basename(os.path.normpath(image_path))
+    img_name_aug = img_name.replace('.jpg', '_aug.jpg')
     org_label_path = os.path.join('../Data/Pages/Base/labels/all_org', img_name.replace('jpg', 'txt'))
     target_words_folder = '../Data/Words/Augmented/'
     target_pages_folder = '../Data/Pages/Augmented/'
     split_folder = os.path.normpath(image_path).split(os.path.sep)[-2]
 
-    if random_int == 0:
-        rotation = random.choice([i for i in range(-10, 10) if i not in range(-1, 1)])
+    print('Augmenting by: ' + random_aug + ' image: ' + img_name)
+    if random_aug == 'rotation':
+        rotation = random.choice([i for i in range(-25, 25) if i not in range(-5, 5)])
         image, shifts_x1, shifts_y1, shifts_x2 = rotate(image, rotation)
         polygons = read_base_labels(org_label_path)
         polygons = rotate_polygons(polygons, shifts_x1, shifts_y1, shifts_x2)
         yolo_labels, bounding_boxes = make_labels_and_bounding_boxes(polygons, image.shape[1], image.shape[0])
-        save_labels_to_txt(image_path, yolo_labels, target_pages_folder, split_folder)
+        save_labels_to_txt(image_path, yolo_labels, target_pages_folder, split_folder, copy_image_to_split=True)
         save_word_images(image, image_path, bounding_boxes, target_words_folder, split_folder)
-        save_image(image, target_pages_folder, split_folder, img_name)
+        save_image(image, target_pages_folder, split_folder, img_name_aug)
 
-    elif random_int == 1:
-        noise_percent = random.choice(np.linspace(0.004, 0.02, 16))
+    elif random_aug == 'noise':
+        noise_percent = random.choice(np.linspace(0.01, 0.08, 14))
         s_to_p_ratio = random.choice(np.linspace(0.25, 0.75, 25))
         image = salt_and_pepper_noise(image, noise_percent, s_to_p_ratio)
         copy_base_page_label(img_name, target_pages_folder, split_folder)
+        copy_base_page_image(img_name, target_pages_folder, split_folder)
         polygons = read_base_labels(org_label_path)
         _, bounding_boxes = make_labels_and_bounding_boxes(polygons, image.shape[1], image.shape[0])
         save_word_images(image, image_path, bounding_boxes, target_words_folder, split_folder)
-        save_image(image, target_pages_folder, split_folder, img_name)
+        save_image(image, target_pages_folder, split_folder, img_name_aug)
 
-    elif random_int == 2:
-        ksize = random.randint(5, 50)
+    elif random_aug == 'blur':
+        ksize = 0
+        while ksize % 2 == 0:
+            ksize = random.randint(5, 50)
         image = cv.GaussianBlur(image, (ksize, ksize), 0)
         copy_base_page_label(img_name, target_pages_folder, split_folder)
+        copy_base_page_image(img_name, target_pages_folder, split_folder)
         polygons = read_base_labels(org_label_path)
         _, bounding_boxes = make_labels_and_bounding_boxes(polygons, image.shape[1], image.shape[0])
         save_word_images(image, image_path, bounding_boxes, target_words_folder, split_folder)
-        save_image(image, target_pages_folder, split_folder, img_name)
+        save_image(image, target_pages_folder, split_folder, img_name_aug)
 
-    elif random_int == 3:
-        size = random.randint(540, 720)
+    elif random_aug == 'downsizing':
+        size = random.randint(420, 720)
         height, width = image.shape[:2]
         if height >= width:
             ratio = size / float(height)
@@ -100,45 +113,94 @@ def random_augmentation(image_path):
         image = cv.resize(image, dim, interpolation=cv.INTER_AREA)
         polygons = read_base_labels(org_label_path)
         polygons = resize_polygons(polygons, ratio)
-        _, bounding_boxes = make_labels_and_bounding_boxes(polygons, image.shape[1], image.shape[0])
+        yolo_labels, bounding_boxes = make_labels_and_bounding_boxes(polygons, image.shape[1], image.shape[0])
+        save_labels_to_txt(image_path, yolo_labels, target_pages_folder, split_folder, copy_image_to_split=True)
         save_word_images(image, image_path, bounding_boxes, target_words_folder, split_folder)
-        save_image(image, target_pages_folder, split_folder, img_name)
+        save_image(image, target_pages_folder, split_folder, img_name_aug)
 
-    elif random_int == 4:
+    elif random_aug == 'removing rows, columns':
         height, width, channels = image.shape
         x_removals_count = width//100
         y_removals_count = height//100
         for i in range(x_removals_count):
-            row = random.randint(0, width-1)
-            image[row] = 255
+            row = random.randint(0, height-1)
+            color = random.randint(0, 255)
+            rows = random.randint(0, 4)
+            image[row:row+rows] = color
         for i in range(y_removals_count):
             column = random.randint(0, width-1)
-            image[:, column] = 255
+            color = random.randint(0, 255)
+            cols = random.randint(0, 4)
+            image[:, column:column+cols] = color
+        copy_base_page_label(img_name, target_pages_folder, split_folder)
+        copy_base_page_image(img_name, target_pages_folder, split_folder)
         polygons = read_base_labels(org_label_path)
         _, bounding_boxes = make_labels_and_bounding_boxes(polygons, image.shape[1], image.shape[0])
         save_word_images(image, image_path, bounding_boxes, target_words_folder, split_folder)
-        save_image(image, target_pages_folder, split_folder, img_name)
+        save_image(image, target_pages_folder, split_folder, img_name_aug)
 
-    elif random_int == 5:
-        print(' ')
-    elif random_int == 6:
-        print(' ')
+    elif random_aug == 'saturation':
+        image_pil = convert_to_pil(image)
+        filter = ImageEnhance.Color(image_pil)
+        enhance = random.choice(np.concatenate((np.linspace(0.3, 0.7, 10), np.linspace(1.3, 3, 10))))
+        image_pil = filter.enhance(enhance)
+        image = convert_to_opencv(image_pil)
+        copy_base_page_label(img_name, target_pages_folder, split_folder)
+        copy_base_page_image(img_name, target_pages_folder, split_folder)
+        polygons = read_base_labels(org_label_path)
+        _, bounding_boxes = make_labels_and_bounding_boxes(polygons, image.shape[1], image.shape[0])
+        save_word_images(image, image_path, bounding_boxes, target_words_folder, split_folder)
+        save_image(image, target_pages_folder, split_folder, img_name_aug)
+
+    elif random_aug == 'brightness':
+        image_pil = convert_to_pil(image)
+        filter = ImageEnhance.Brightness(image_pil)
+        enhance = random.choice(np.concatenate((np.linspace(0.3, 0.7, 10), np.linspace(1.3, 3, 10))))
+        image_pil = filter.enhance(enhance)
+        image = convert_to_opencv(image_pil)
+        copy_base_page_label(img_name, target_pages_folder, split_folder)
+        copy_base_page_image(img_name, target_pages_folder, split_folder)
+        polygons = read_base_labels(org_label_path)
+        _, bounding_boxes = make_labels_and_bounding_boxes(polygons, image.shape[1], image.shape[0])
+        save_word_images(image, image_path, bounding_boxes, target_words_folder, split_folder)
+        save_image(image, target_pages_folder, split_folder, img_name_aug)
+
+    elif random_aug == 'contrast':
+        image_pil = convert_to_pil(image)
+        filter = ImageEnhance.Contrast(image_pil)
+        enhance = random.choice(np.concatenate((np.linspace(0.3, 0.7, 10), np.linspace(1.3, 3, 10))))
+        image_pil = filter.enhance(enhance)
+        image = convert_to_opencv(image_pil)
+        copy_base_page_label(img_name, target_pages_folder, split_folder)
+        copy_base_page_image(img_name, target_pages_folder, split_folder)
+        polygons = read_base_labels(org_label_path)
+        _, bounding_boxes = make_labels_and_bounding_boxes(polygons, image.shape[1], image.shape[0])
+        save_word_images(image, image_path, bounding_boxes, target_words_folder, split_folder)
+        save_image(image, target_pages_folder, split_folder, img_name_aug)
+
     else:
-        sys.exit('Random number for augmentation not in range')
+        sys.exit(random_aug + ' is not a valid augmentation')
 
     return image
 
 
-def augment_folder(folder_path):
+def augmenting_folder(folder_path):
+    img_count = len([name for name in os.listdir(folder_path)])
+    img_counter = 0
     for image_path in os.listdir(folder_path):
+        img_counter += 1
         path = os.path.join(folder_path, image_path)
-        print(path)
+        print(str(img_counter) + '/' + str(img_count) + ' in ' + folder_path)
         random_augmentation(path)
 
 
-random.seed(65)
-drop_splits_target_folders('../Data/Pages/Augmented/')
-drop_splits_target_folders('../Data/Words/Augmented/')
-create_splits_target_folders('../Data/Pages/Augmented/')
-create_splits_target_folders('../Data/Words/Augmented/')
-copy_base_word_labels('../Data/Words/Augmented/')
+if __name__ == '__main__':
+    random.seed(65)
+    drop_splits_target_folders('../Data/Pages/Augmented/')
+    drop_splits_target_folders('../Data/Words/Augmented/')
+    create_splits_target_folders('../Data/Pages/Augmented/')
+    create_splits_target_folders('../Data/Words/Augmented/')
+    copy_base_word_labels('../Data/Words/Augmented/')
+    augmenting_folder('../Data/Pages/Base/images/val')
+    augmenting_folder('../Data/Pages/Base/images/train')
+    augmenting_folder('../Data/Pages/Base/images/test')
