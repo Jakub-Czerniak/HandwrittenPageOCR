cef7c2a7c4750fe4659cbcbda948f7dc43062198
jakub-czerniak <j.deadyet@protonmail.com>
2024-03-06 12:36:23 +0100

    Training and tuning params

--- WordDetection/Training.py
+++ WordDetection/Training.py
@@ -3,4 +3,14 @@ from ultralytics import YOLO
 model = YOLO('yolov8x.pt')
 
 #python train.py --img 1088 --batch 8 --epochs 150 --data ./data/ocr_handwriting_data.yaml --weights [string]::Empty --cfg yolov5s.yaml --name yolov5s_results
-results = model.train(data='base.yaml', epochs=1, imgsz=1088, batch=8)
+results = model.train(data='augmented.yaml', epochs=200, imgsz=1088, batch=8, hsv_h=0, hsv_s=0, hsv_v=0,
+                      degrees=0, translate=0, scale=0, shear=0, perspective=0, flipud=0, fliplr=0,
+                      mosaic=0, mixup=0, copy_paste=0, erasing=0)
+
+results = model.train(data='preprocessed.yaml', epochs=200, imgsz=1088, batch=8, hsv_h=0, hsv_s=0, hsv_v=0,
+                      degrees=0, translate=0, scale=0, shear=0, perspective=0, flipud=0, fliplr=0,
+                      mosaic=0, mixup=0, copy_paste=0, erasing=0)
+
+results = model.train(data='base.yaml', epochs=200, imgsz=1088, batch=8, hsv_h=0, hsv_s=0, hsv_v=0,
+                      degrees=0, translate=0, scale=0, shear=0, perspective=0, flipud=0, fliplr=0,
+                      mosaic=0, mixup=0, copy_paste=0, erasing=0)
--- WordDetection/Tuning.py
+++ WordDetection/Tuning.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO('./yolov8x.pt')
 
-model.tune(data='./base.yaml', epochs=30, iterations=1, optimizer='AdamW', plots=False, save=False, val=False)
+model.tune(data='base.yaml', epochs=30, iterations=30, optimizer='AdamW', plots=False, save=False, val=False)
